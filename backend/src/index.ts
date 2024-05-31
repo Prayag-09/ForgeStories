11d0c90877a9c7d59e4b79cc86e397c09fab68e0
Prayag <prayagtushar2016@gmail.com>
2024-05-31 17:01:38 +0530

    Prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,11 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/extension';
+import { withAccelerate } from '@prisma/extension-accelerate';
+require("dotenv").config();
+
+const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
 
 const app = new Hono()
 
@@ -7,4 +14,4 @@ const app = new Hono()
 
 
 
-export default app
+export default app;
